172fdf9cd9275cd44b7abebd1821f200c6c16f86
Unrealistic_Pug <55397385+Alexandr-Puglord@users.noreply.github.com>
2020-04-18 21:22:02 -0700

    Version 2 of the game
    This is the final version of the game

---
+++ Fit-Blocks-V2/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  //this will go from Menu 0 to GameScene 1 
+                                                                               //The build will 
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exiting Game"); //this is a confirmation message that will popup in the unity engine 
+        Application.Quit(); //this will in fact exit the program 
+    }
+
+}
