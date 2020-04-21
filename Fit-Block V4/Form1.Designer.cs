c9fae47f9fc81053901ff18c79d64fe856bf38ce
Unrealistic_Pug <55397385+Alexandr-Puglord@users.noreply.github.com>
2020-04-20 18:20:54 -0700

    Fit-Block-V4
    This is the final version with added score system

---
+++ Fit-Block V4/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Tetris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UTLaunchButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UTLaunchButton
+            // 
+            this.UTLaunchButton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.UTLaunchButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveBorder;
+            this.UTLaunchButton.FlatAppearance.BorderSize = 3;
+            this.UTLaunchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UTLaunchButton.Location = new System.Drawing.Point(259, 109);
+            this.UTLaunchButton.Name = "UTLaunchButton";
+            this.UTLaunchButton.Size = new System.Drawing.Size(181, 58);
+            this.UTLaunchButton.TabIndex = 0;
+            this.UTLaunchButton.Text = "Launch →";
+            this.UTLaunchButton.UseVisualStyleBackColor = false;
+            this.UTLaunchButton.Click += new System.EventHandler(this.UTLaunchButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Unity Tetris";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(372, 54);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Game Launcher";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(77, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(305, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "By Alexandr Pokorsky and Tyler Labelle";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::Tetris.Properties.Resources.texture_01;
+            this.ClientSize = new System.Drawing.Size(475, 190);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UTLaunchButton);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(493, 237);
+            this.MinimumSize = new System.Drawing.Size(493, 237);
+            this.Name = "Form1";
+            this.Text = "AP-TL Game Launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UTLaunchButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Fit-Block V4/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+
+namespace Tetris
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void UTLaunchButton_Click(object sender, EventArgs e)
+        {
+            UnityTetris UT = new UnityTetris();
+
+            UT.RunGame();
+        }
+    }
+
+    public abstract class Game
+    {
+        //Variables
+        public string CurrentDir = Directory.GetCurrentDirectory();
+        
+        public string Name { get; set; }
+
+        //Constructor
+        public Game(string N)
+        {
+            this.Name = N;
+        }
+
+        public abstract void RunGame();
+    }
+
+    public class UnityTetris : Game
+    {
+        public UnityTetris(string name = "UnityTetris") : base(name) { }
+
+        public override void RunGame()
+        {
+            Process.Start(CurrentDir + "\\Fit-Blocks Game\\Fit-Blocks-Version-2.exe");
+        }
+    }
+}
